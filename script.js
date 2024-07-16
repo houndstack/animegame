16a31287ff5451102e70075a7d790e1c163fcf55
houndstack <48732916+houndstack@users.noreply.github.com>
2024-07-16 11:23:43 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,167 @@
+function generate(query, variables){
+  // Here we define our query as a multi-line string
+// Storing it in a separate .graphql/.gql file is also possible
+  
+  
+  // Define the config we'll need for our Api request
+  var url = 'https://graphql.anilist.co',
+      options = {
+          method: 'POST',
+          headers: {
+              'Content-Type': 'application/json',
+              'Accept': 'application/json',
+          },
+          body: JSON.stringify({
+              query: query,
+              variables: variables
+          })
+      };
+
+  // Make the HTTP Api request
+  return fetch(url, options).then(handleResponse)
+                     .then(data => {x = data; console.log(data); return data})
+                     .catch(handleError);
+  
+}
+function handleResponse(response) {
+      return response.json().then(function (json) {
+          return response.ok ? json : Promise.reject(json);
+      });
+}
+  
+function handleError(error) {
+      alert('Error, check console');
+      console.error(error);
+}
+
+
+
+async function randomAnime(id){
+    var query = `
+    query ($id: Int) {
+        Media(id: $id){
+          title {
+            romaji
+            english
+            native
+            userPreferred
+          }
+          coverImage {
+            extraLarge
+            large
+            medium
+            color
+          }
+          bannerImage
+          recommendations(sort: RATING_DESC) {
+            edges {
+              node {
+                id
+                rating
+                mediaRecommendation {
+                  coverImage {
+                      extraLarge
+                      large
+                      medium
+                      color
+                  }    
+                  id
+                  title {
+                    romaji
+                    english
+                    native
+                    userPreferred
+                  }
+                }
+              }
+            }
+          }
+        }
+      }
+    `
+    if(id==undefined){
+        input1 = document.getElementById("id1")
+        if (input1 && input1.value) {
+          console.log("value had")
+        }
+        else{
+          input1.value = idArray[Math.floor(Math.random() * idArray.length)];
+
+        }
+        input2 = document.getElementById("id2")
+        if (input2 && input2.value) {
+          console.log("value 2 had")
+        }
+        else{
+          randomId2 = idArray[Math.floor(Math.random() * idArray.length)]
+          variables = {
+            'id': randomId2
+          }
+          response = await generate(query, variables)
+          console.log()
+          targetTitle = document.createElement('span')
+          targetTitle.textContent = 'Target Anime: ' + response['data']['Media']['title']['romaji'];
+          document.getElementById('inputs').appendChild(targetTitle)
+          
+
+        }
+        id = parseInt(input1.value)
+        document.getElementById('id1').disabled = true;
+        document.getElementById('id2').disabled = true;
+    }
+    else{
+      document.getElementById('id1').disabled = false;
+      document.getElementById('id2').disabled = false;
+      counter++
+      if(id==document.getElementById("id2").value){
+        console.log("you winner")
+        document.getElementById("cover").replaceChildren()
+        this.win = document.createElement("div");
+        this.win.textContent = "winner winner" + counter
+        counter = 0
+        document.getElementById("cover").appendChild(this.win)
+        document.getElementById("recommendations").replaceChildren()
+        return;
+      }
+    }
+    
+    
+  
+  
+    variables = {
+        'id': id
+    }
+    response = await generate(query, variables)
+    data = response['data']['Media']
+    console.log(response)
+    var imageUrl = data['coverImage']['large']
+    var coverImage = document.getElementById('cover')
+    this.img = document.createElement("img");
+    this.img.src = imageUrl;
+    coverImage.replaceChildren()
+    coverImage.appendChild(this.img)
+    this.title = document.createElement("h1");
+    this.title.textContent = data['title']['romaji']
+    coverImage.appendChild(this.title)
+    recommendations = data['recommendations']['edges']
+    document.getElementById("recommendations").replaceChildren()
+    for(var i = 0;i<recommendations.length;i++){
+        let img = document.createElement("button");
+        let id = recommendations[i]['node']['mediaRecommendation']['id']
+        img.setAttribute('id', id)
+        img.innerHTML = '<img src=' + recommendations[i]['node']['mediaRecommendation']['coverImage']['medium'] + ' />';
+        //img.src = recommendations[i]['node']['mediaRecommendation']['coverImage']['medium']
+        console.log(img.getAttribute('id'))
+        img.onclick= function() {console.log(img);randomAnime(img.getAttribute('id'))}
+        let title = document.createElement("div")
+        title.textContent = recommendations[i]['node']['mediaRecommendation']['title']['romaji'] + ': ' + recommendations[i]['node']['rating']
+        let compartment = document.createElement("div")
+        compartment.appendChild(img)
+        compartment.appendChild(title)
+        compartment.className += "recommendationButton"
+        document.getElementById("recommendations").appendChild(compartment);
+    }
+    
+}
+
+var counter = 0;
